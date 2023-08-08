5a5f817eb40b6ec0e6b1d896f937048126e6be9e
Cameron Goddard <camerongoddard@me.com>
2023-08-07 22:56:51 -0400

    reimplement composited icons

--- Sources/CSMenu/CSPopUpButton.swift
+++ Sources/CSMenu/CSPopUpButton.swift
@@ -15,10 +15,24 @@ public class CSPopUpButton: NSButton {
     var menuItems: [CSMenuItem] = []
     private var isShown = false
     
+    private var icon = NSImage()
+    private var texture: NSImage?
+    private var texturePressed: NSImage?
+    
     public var lastItem: CSMenuItem?
     
     private let base = NSImage(named: "cs_button")!
     private let basePressed = NSImage(named: "cs_button_pressed")!
+    
+    public init(icon: NSImage, x: Int, y: Int) {
+        self.icon = icon
+        
+        self.texture = CSPopUpButton.addIcon(base: base, icon: icon, x: CGFloat((x+3))*SCALE, y: CGFloat((y+3))*SCALE)
+        self.texturePressed = CSPopUpButton.addIcon(base: basePressed, icon: icon, x: CGFloat((x+4))*SCALE, y: CGFloat((y+2))*SCALE)
+        
+        super.init(frame: NSRect(x: 100.0, y: 0.0, width: 31 * SCALE, height: 24 * SCALE))
+        setup()
+    }
     
     public override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
@@ -50,10 +64,10 @@ public class CSPopUpButton: NSButton {
     public override func mouseDown(with event: NSEvent) {
         // TODO: Add monitors if no mouseDown override
         if self.state == .on {
-            self.image = self.base
+            self.image = self.texture
             self.state = .off
         } else {
-            self.image = self.basePressed
+            self.image = self.texturePressed
             self.state = .on
         }
         
@@ -72,5 +86,16 @@ public class CSPopUpButton: NSButton {
             popUpMenu = CSMenu(items: self.menuItems)
             popUpMenu?.popUp(at: screenOrigin, in: superview!)
         }
+    }
+    
+    private static func addIcon(base: NSImage, icon: NSImage, x: CGFloat, y: CGFloat) -> NSImage {
+        let image = NSImage(size: base.size)
+        image.lockFocus()
+        
+        base.draw(at: NSPoint.zero, from: NSRect(origin: NSPoint.zero, size: base.size), operation: .copy, fraction: 1.0)
+        icon.draw(at: NSPoint(x: x, y: y), from: NSRect(origin: NSPoint.zero, size: icon.size), operation: .sourceOver, fraction: 1.0)
+
+        image.unlockFocus()
+        return image
     }
 }
